ec9487062a55ac20924714b69fa5091d03f4f2d8
Francisco José Castillo Severino <delegadocastillo@hotmail.com>
2018-11-30 15:29:21 -0400

    Mas viastas

---
+++ VistasPostAdd/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VistasPostAdd.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
