d89ee1b944fda51764ae539d1df4dfc1c5abaf5d
Javier Ismael De Jesus Martinez <ismael.dt@hotmail.com>
2018-12-01 20:46:10 -0400

    Login: Ready

--- VistasPostAdd/Controllers/LoginController.cs
+++ VistasPostAdd/Controllers/LoginController.cs
@@ -3,16 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using PostAds.Models;
+using VistasPostAdd.ViewModel;
 
 namespace VistasPostAdd.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly SignInManager<AppUser> signInManager;
+
+        public LoginController(SignInManager<AppUser> signInManager)
+        {
+            this.signInManager = signInManager;
+        }
         // GET: Login
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await signInManager.PasswordSignInAsync(model.Email, model.Password, false, false);
+                if (user.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+            }
+            return RedirectToAction("Index", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
         }
 
     }
