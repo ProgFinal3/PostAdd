8a1a3e82341a51a1424c7406bee5503b44fc077d
Francisco José Castillo Severino <delegadocastillo@hotmail.com>
2018-12-05 15:05:52 -0400

    cambios en vista detalle y controlador home mas css

--- VistasPostAdd/Controllers/HomeController.cs
+++ VistasPostAdd/Controllers/HomeController.cs
@@ -24,15 +24,10 @@ namespace VistasPostAdd.Controllers
             return View(dbContex.Anuncio.Include(x => x.Imagen).ToList());
         }
 
-        public IActionResult Datalle()
+        public IActionResult Detalle()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
